f64b314f29bd458b582b083a0a9904c3674a0e0d
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-07-07 11:51:10 -0600

    Update Jerseys.java

--- CCC 2015/Jerseys.java
+++ CCC 2015/Jerseys.java
@@ -1,19 +1,10 @@
-//Got 12/15 on the Online Grade due to inefficency 
-
 import java.util.*;
 
 public class Jerseys {
-    private static boolean smaller(String shirtIn, String next){
-        if (next.equals("S")){
-            return true;
-        }
-        switch (shirtIn){
-            case "L":
-                return (next.equals("M"));
-        }
-        return false;
-    }
     private static boolean fits(String available, String desired) {
+        if (available == null) {
+            return false;
+        }
         if (desired.equals("S")) {
             return true;
         }
@@ -26,46 +17,26 @@ public class Jerseys {
         return true;
     }
     public static void main(String[] args){
-            Scanner in = new Scanner(System.in);
-            int numJerseys = Integer.parseInt(in.nextLine());
-            int numAthletes = Integer.parseInt(in.nextLine());
-            HashMap<Integer, String> record = new HashMap<>();
+        Scanner in = new Scanner(System.in);
 
-            String[] sizes = new String[numJerseys];
-            ArrayList<String> requests = new ArrayList<>();
-            ArrayList<Integer> checked = new ArrayList<>();
-            for (int i = 0; i < numJerseys; i++) {
-                sizes[i] = in.nextLine();
+        int numJerseys = Integer.parseInt(in.nextLine());
+        int numAthletes = Integer.parseInt(in.nextLine());
+
+        String[] jerseys = new String[numJerseys];
+        for (int i = 0; i < numJerseys; i++){
+            jerseys[i] = in.nextLine();
+        }
+        int counter = 0;
+        for (int i = 0; i < numAthletes; i++){
+            String[] request = in.nextLine().split(" ");
+            String size = request[0];
+            int numIndex = Integer.parseInt(request[1]) - 1;
+
+            if (fits(jerseys[numIndex], size) && jerseys[numIndex] != null ){
+                jerseys[numIndex] = null;
+                counter++;
             }
-            for (int i = 0; i < numAthletes; i++) {
-                String[] request = in.nextLine().split(" ");
-                int num = Integer.parseInt(request[1]);
-                String size = request[0];
-                if (record.keySet().contains(num)){
-                    if (smaller(record.get(num), size)){
-                        record.replace(num, record.get(num), size);
-                    }
-                } else{
-                    record.put(num, size);
-                }
-            }
-            for (int key:record.keySet()){
-                requests.add(record.get(key) + " " + key);
-            }
-            int satisfied = 0;
-            for (int i = 0; i < requests.size(); i++) {
-                int num = Integer.parseInt(requests.get(i).split(" ")[1]);
-                String size = requests.get(i).split(" ")[0];
-                try {
-                    String shirtSize = sizes[num - 1];
-                    if (fits(shirtSize, size) && !checked.contains(num - 1)) {
-                        satisfied++;
-                        checked.add(num - 1);
-                    }
-                } catch (ArrayIndexOutOfBoundsException aioobe) {
-                    aioobe.printStackTrace();
-                }
-            }
-            System.out.println(satisfied);
+        }
+        System.out.println(counter);
     }
 }
