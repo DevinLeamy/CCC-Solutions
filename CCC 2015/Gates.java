bf83386aa1f43ff8d327637610039cb71859b20b
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-07-07 11:24:37 -0600

    Add files via upload

---
+++ CCC 2015/Gates.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Gates {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int numGates = Integer.parseInt(in.nextLine());
+        int numPlanes = Integer.parseInt(in.nextLine());
+        TreeSet<Integer> docked = new TreeSet<>();
+        int counter = 0;
+        for (int i = 1; i <= numGates; i++){
+            docked.add(i);
+        }
+        for (int i = 0; i < numPlanes; i++){
+            int gate = Integer.parseInt(in.nextLine());
+            if (docked.floor(gate) == null){
+                break;
+            }
+            docked.remove(docked.floor(gate));
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
