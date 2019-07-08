64dcc68fcd10db9fa69e5d01e38d6737d8bcb90e
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-07-08 12:08:36 -0600

    Update PiDay.java

--- CCC 2015/PiDay.java
+++ CCC 2015/PiDay.java
@@ -3,47 +3,25 @@
 import java.util.*;
 
 public class PiDay {
-    public static int position(int pieces, int pos, int numPeople, int consumed, int counter, int[] people){
-        if (pos == (numPeople)){
-            if (consumed == pieces){
-                counter += 1;
-                return counter;
-            } else{
-                return counter;
-            }
-        }
-        else if (consumed > pieces){
+    private static int givePie(int pieces, int people, int given, int served, int counter, int lastPiece){
+        if (pieces == given && served == people){
+            counter++;
             return counter;
-        } else {
-            int max = ((pieces - consumed) / (numPeople - pos)) + 1;
-            if (pos == 0){
-                for (int i = 1; i < max ; i++) {
-                    people[pos] = i;
-                    counter = position(pieces, pos + 1, numPeople, consumed + i, counter, people);
-                }
-            } else {
-                for (int i = people[pos - 1]; i < max; i++) {
-                    people[pos] = i;
-                    if (people[pos - 1] <= people[pos]) {
-                        counter = position(pieces, pos + 1, numPeople, consumed + i, counter, people);
-                    }
-                }
-            }
+        } else if (served == people){
+            return counter;
+        }
+        int max = ((pieces - given) / (people - served));
+        for (int i = lastPiece; i < (max + 1); i++){
+            counter = givePie(pieces, people, given + i, served + 1, counter, i);
         }
         return counter;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int pieces = Integer.parseInt(in.nextLine());
-        int numPeople = Integer.parseInt(in.nextLine());
-        int[] people = new int[numPeople];
-        Arrays.fill(people, pieces);
-        if (pieces == numPeople) {
-            System.out.println(1);
-        } else if (numPeople == 1) {
-            System.out.println(1);
-        } else {
-            System.out.println(position(pieces, 0, numPeople, 0, 0, people));
-        }
+        int people = Integer.parseInt(in.nextLine());
+        System.out.println(givePie(pieces, people, 0, 0, 0, 1));
+
+
     }
 }
