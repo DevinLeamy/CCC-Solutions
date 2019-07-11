8c5048062e16b5be18680e9e24e4f25e08d4f7f5
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-07-11 10:19:52 -0600

    Update RuleOfThree.java

--- CCC 2019/RuleOfThree.java
+++ CCC 2019/RuleOfThree.java
@@ -1,51 +1,72 @@
-// 7/15 On Online Grader because of Efficiency
-
 import java.util.*;
 
 public class RuleOfThree {
-    private static String[] newArray(String original, ArrayList<String[]> arrays, String newString, int startIndex, int rule){
-        if (arrays.size() > 0) {
-            String[] output = new String[arrays.get(0).length + 1];
-            for (String[] array : arrays) {
-                if (array[array.length - 1].split(" ")[2].equals(original)) {
-                    for (int i = 0; i < array.length; i++) {
-                        output[i] = array[i];
-                    }
-                    output[output.length - 1] = rule + " " + (startIndex + 1) + " " + newString;
-
-                    return output;
-                }
-            }
-        }
-        String[] output = new String[1];
-        output[0] = rule + " " + (startIndex + 1) + " " + newString;
-        return output;
-    }
-    private static void addValues(String original, String checkFor, String replaceWith, ArrayList<String> addHere, ArrayList<String[]> steps, int rule){
-        try {
-            for (int i = 0; i < original.length(); i++) {
-                String sub = original.substring(i, i + checkFor.length());
-                if (sub.equals(checkFor)) {
-                    String newString = convert(i, i + checkFor.length(), replaceWith, original);
-                    String[] step = newArray(original, steps, newString, i, rule);
-                    addHere.add(newString);
-                    steps.add(step);
-                }
-            }
-        } catch (StringIndexOutOfBoundsException sioobe){}
-    }
-    private static String convert(int startIndex, int endIndex, String changeTo, String original){
+    private static boolean found = false;
+    private static String getNewString(String original, int startIndex, int endIndex, String changeTo){
         String output = "";
         for (int i = 0; i < original.length(); i++){
-            if (i < endIndex && i >= startIndex){
-                if (i == (endIndex - 1)) {
-                    output += changeTo;
-                }
-            } else {
-                output += String.valueOf(original.charAt(i));
+            if (i == startIndex){
+                output += changeTo;
+                i = (endIndex - 1);
+            } else{
+                output += original.charAt(i);
             }
         }
         return output;
+    }
+    private static ArrayList<String> getMoves(String[] ruleOne, String[] ruleTwo, String[] ruleThree, String current){
+        ArrayList<String> moves = new ArrayList<>();
+        for (int i = 0; i < current.length(); i++){
+            try {
+                String ruleOneSubString = current.substring(i, i + ruleOne[0].length());
+                if (ruleOneSubString.equals(ruleOne[0])){
+                    moves.add("1 " + i + " " + (i + ruleOne[0].length()) + " " + getNewString(current, i, i + ruleOne[0].length(), ruleOne[1]));
+                }
+            } catch (StringIndexOutOfBoundsException sioobe){}
+            try {
+                String ruleTwoSubString = current.substring(i, i + ruleTwo[0].length());
+                if (ruleTwoSubString.equals(ruleTwo[0])){
+                    moves.add("2 " + i + " " + (i + ruleTwo[0].length()) + " " + getNewString(current, i, i + ruleTwo[0].length(), ruleTwo[1]));
+                }
+            } catch (StringIndexOutOfBoundsException sioobe){}
+            try {
+                String ruleThreeSubString = current.substring(i, i + ruleThree[0].length());
+                if (ruleThreeSubString.equals(ruleThree[0])){
+                    moves.add("3 " + i + " " + (i + ruleThree[0].length()) + " " + getNewString(current, i, i + ruleThree[0].length(), ruleThree[1]));
+                }
+            } catch (StringIndexOutOfBoundsException sioobe){}
+        }
+        return moves;
+    }
+    private static void findSolution(String[] ruleOne, String[] ruleTwo, String[] ruleThree,
+                                     String current, String desired, int movesTaken, int movesAllowed, String[] solution, ArrayList<String> visited){
+        if (found){
+            return;
+        }
+        if (movesTaken == movesAllowed){
+            if (current.equals(desired)){
+                for (String move: solution){
+                    System.out.println(move);
+                }
+                found = true;
+            }
+            return;
+        }
+        visited.add(current + " " + movesTaken);
+        ArrayList<String> moves = getMoves(ruleOne, ruleTwo, ruleThree, current);
+        if (moves.size() > 0){
+            for (String move: moves){
+                String[] split = move.split(" ");
+                if (!visited.contains(split[3] + " " + (movesTaken+1))) {
+                    String addTo = split[0] + " " + (Integer.parseInt(split[1]) + 1) + " " + split[3];
+                    solution[movesTaken] = addTo;
+                    findSolution(ruleOne, ruleTwo, ruleThree, split[3], desired, movesTaken + 1, movesAllowed, solution, visited);
+                    solution[movesTaken] = "";
+                }
+            }
+        } else {
+            return;
+        }
     }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
@@ -53,52 +74,8 @@ public class RuleOfThree {
         String[] ruleTwo = in.nextLine().split(" ");
         String[] ruleThree = in.nextLine().split(" ");
         String[] values = in.nextLine().split(" ");
-        int numMoves = Integer.parseInt(values[0]);
-        String initial = values[1];
-        String result = values[2];
-
-        ArrayList<String[]> moves = new ArrayList<>();
-        ArrayList<String> queue = new ArrayList<>();
-        ArrayList<String> checked = new ArrayList<>();
-        ArrayList<String> next = new ArrayList<>();
-        checked.add(initial);
-        queue.add(initial);
-        int counter = 0;
-
-        while (counter < numMoves){
-            for (String value: queue){
-                if (value.contains(ruleOne[0]) || value.contains(ruleTwo[0]) || value.contains(ruleThree[0])){
-                    addValues(value, ruleOne[0], ruleOne[1], next, moves, 1);
-                    addValues(value, ruleTwo[0], ruleTwo[1], next, moves, 2);
-                    addValues(value, ruleThree[0], ruleThree[1], next, moves, 3);
-                }
-            }
-            queue = new ArrayList<>();
-            for (String go: next){
-                if (!checked.contains(go)){
-                    checked.add(go);
-                    queue.add(go);
-                }
-            }
-            next = new ArrayList<>();
-            counter++;
-            ArrayList<String[]> hold = new ArrayList<>();
-            if (moves.size() > 0) {
-                for (String[] array : moves) {
-                    if (array.length == (counter - 1)) {
-                        hold.add(array);
-                    }
-                }
-            }
-            moves.removeAll(hold);
-        }
-        for (String[] array: moves){
-            if (array[array.length - 1].split(" ")[2].equals(result)){
-                for (String step: array){
-                    System.out.println(step);
-                }
-                break;
-            }
-        }
+        String[] solution = new String[Integer.parseInt(values[0])];
+        ArrayList<String> visited = new ArrayList<>();
+        findSolution(ruleOne, ruleTwo, ruleThree, values[1], values[2], 0, Integer.parseInt(values[0]), solution, visited);
     }
 }
