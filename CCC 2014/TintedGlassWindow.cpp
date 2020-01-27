7771f0ae3e61c84a8cb8b3eca228bbb7ef20ac5e
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2020-01-27 16:11:42 -0700

    Update TintedGlassWindow.cpp

--- CCC 2014/TintedGlassWindow.cpp
+++ CCC 2014/TintedGlassWindow.cpp
@@ -12,45 +12,44 @@
 #define loop(h) for(int i = 0; i < h; i++)
 
 using namespace std;
-
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int n; ll T; ll count = 0; ll rt, rb, cl, cr, t;
-	vector<pair<ll, ll>> tintsX; vector<pair<ll, pair<ll, ll>>> tintY;
+	vector<pair<ll, pair< pair<ll, ll>, ll> >> tintsX;
 	cin >> n >> T;
 	loop(n) {
 		cin >> cl >> rb >> cr >> rt >> t;
-		tintsX.pb({cl, i}); tintsX.pb({cr, i});
-		tintY.pb({rb, {t, i}}); tintY.pb({rt, {-t, i}});
+		tintsX.pb({cl, { {rb, rt}, t }}); tintsX.pb({cr, { {rb, rt}, -t }});
 	}
 	sort(tintsX.begin(), tintsX.end());
-	sort(tintY.begin(), tintY.end());
 	vector<ll> ids;
+	vector< pair<ll, ll> > tints;
 	for (int i = 0; i < tintsX.size()-1; i++) {
-		pair<ll, ll> val = tintsX[i];
-		ll x = val.first; ll id = val.second;
-		
-		if (find(ids.begin(), ids.end(), id) != ids.end()) {
-			ids.erase(find(ids.begin(), ids.end(), id));
-		} else {
-			ids.pb(id);
-		}
+		pair<ll, pair< pair<ll, ll>, ll> > item = tintsX[i];
+		ll x = item.first;
 		ll last = -1; ll currentTint = 0;
-		for (int j = 0; j < tintY.size(); j++) {
-			if (find(ids.begin(), ids.end(), tintY[j].second.second) != ids.end()) {
-				pair<ll, pair<ll, ll>> h = tintY[j];
-				if (last == -1) {
-					last = h.first;
-					currentTint += h.second.first;
-				} else {
-					if (currentTint >= T) {
-						count += (abs(last - h.first)) * (abs(x - tintsX[i+1].first));
-					}
-					last = h.first;
-					currentTint += h.second.first; 
+		if (item.second.second < 0) {
+			tints.erase( find(tints.begin(), tints.end(), make_pair(item.second.first.first, -item.second.second) ) );
+			tints.erase( find(tints.begin(), tints.end(), make_pair(item.second.first.second, item.second.second) ) );
+		} else {
+			tints.pb({item.second.first.first, item.second.second});
+			tints.pb({item.second.first.second, -item.second.second});
+			sort(tints.begin(), tints.end());
+		}
+		for (int j = 0; j < tints.size(); j++) {
+			pair<ll, ll> h = tints[j];
+			if (last == -1) {
+				last = h.first;
+				currentTint += h.second;
+			} else {
+				if (currentTint >= T) {
+					count += (abs(last - h.first)) * (abs(x - tintsX[i+1].first));
 				}
+				last = h.first;
+				currentTint += h.second;
 			}
 		}
+		
 	}
 	cout << count << endl;
 	return 0;
