29d6d03f6068ca67c07093942900698368b55ab4
DevinLeamy <devinleamy@gmail.com>
2020-06-29 02:15:29 -0600

    Update SwappingSeats.cpp

--- CCC 2020/SwappingSeats.cpp
+++ CCC 2020/SwappingSeats.cpp
@@ -14,31 +14,21 @@
 using namespace std;
 char _;
 
+string s;
+
 int swaps(int a, int b) {
-	return min(a, b) + (max(a, b) - min(a, b));
+	return max(a, b);
 }
 
-int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	string s; cin >> s;
-	int as = 0;
-	int bs = 0;
-	for (int i = 0; i < s.length(); i++) {
-		if (s[i] == 'A') { as++; }
-		else if (s[i] == 'B') { bs++; }
-	}
-	int aLp = 0;
-	int aRp = as-1;
-	int bLp = as;
-	int bRp = as + bs - 1;
+int check(int aLp, int aRp, int bLp, int bRp, int aS, int aE, int bS, int bE) {
 	int aM = 0;
 	int bM = 0;
 	int c = 0;
-	for (int i = 0; i < as; i++) {
+	for (int i = aS; i < aE; i++) {
 		if (s[i] == 'B') { aM++; }
 		else if (s[i] == 'C') { c++; }
 	}
-	for (int i = as; i < as + bs; i++) {
+	for (int i = bS; i < bE; i++) {
 		if (s[i] == 'A') { bM++; }
 		else if (s[i] == 'C') { c++; }
 	}
@@ -48,55 +38,28 @@ int main() {
 		bRp = (bRp + 1) % s.length();
 		if (s[aLp] == 'B') { aM--; }
 		else if (s[aLp] == 'C') { c--; }
-
 		if (s[aRp] == 'B') { aM++; }
 		else if (s[aRp] == 'C') { c++; }
-		
 		if (s[bLp] == 'A') { bM--; }
 		else if (s[bLp] == 'C') { c--; }
-		
 		if (s[bRp] == 'A') { bM++; }
 		else if (s[bRp] == 'C') { c++; }
-		
 		min = std::min(swaps(aM, bM) + c, min);
 		bLp = (bLp + 1) % s.length();
 		aLp = (aLp + 1) % s.length();
 	}
-	aLp = bs;
-	aRp = as + bs - 1;
-	bLp = 0;
-	bRp = bs - 1;
-	aM = 0;
-	bM = 0;
-	c = 0;
-	for (int i = bs; i < as + bs; i++) {
-		if (s[i] == 'B') { aM++; }
-		else if (s[i] == 'C') { c++; }
-	}
-	for (int i = 0; i < bs; i++) {
-		if (s[i] == 'A') { bM++; }
-		else if (s[i] == 'C') { c++; }
-	}
-	min = std::min(swaps(aM, bM) + c, min);
+	return min;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> s;
+	int as = 0;
+	int bs = 0;
 	for (int i = 0; i < s.length(); i++) {
-		aRp = (aRp + 1) % s.length();
-		bRp = (bRp + 1) % s.length();
-		if (s[aLp] == 'B') { aM--; }
-		else if (s[aLp] == 'C') { c--; }
-
-		if (s[aRp] == 'B') { aM++; }
-		else if (s[aRp] == 'C') { c++; }
-		
-		if (s[bLp] == 'A') { bM--; }
-		else if (s[bLp] == 'C') { c--; }
-		
-		if (s[bRp] == 'A') { bM++; }
-		else if (s[bRp] == 'C') { c++; }
-		
-		min = std::min(swaps(aM, bM) + c, min);
-		bLp = (bLp + 1) % s.length();
-		aLp = (aLp + 1) % s.length();
+		if (s[i] == 'A') { as++; }
+		else if (s[i] == 'B') { bs++; }
 	}
-	cout << min << endl;
+	cout << min(check(0, as - 1, as, as + bs - 1, 0, as, as, as + bs), check(bs, as + bs - 1, 0, bs - 1, bs, as + bs, 0, bs));
 	return 0;
 }
